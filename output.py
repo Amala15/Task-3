3f5a70bc0fb3f993a746edb44f4e8fbe964aefe6
Amala15 <63283936+Amala15@users.noreply.github.com>
2020-04-09 11:39:12 +0530

    Add files via upload
    1.Assigning elements to different list
    2.Accessing elements from a tuple (indexing.py , negative indexing.py , slicing.py , output.py)
    3.Deleting different dictionary elements (All 3 methods + output.py )

---
+++ output.py
@@ -0,0 +1,12 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: F:/Amala Varghese/My Captain-Online Course/AI (April Course)/3.Deleting different dictionary elements/Method-using diff dictionary components.py
+{1: 'Geeks', 2: 'For'}
+>>> 
+= RESTART: F:/Amala Varghese/My Captain-Online Course/AI (April Course)/3.Deleting different dictionary elements/Method-using mylist.py
+{1: 'Geeks', 3: 'Geeks'}
+>>> 
+= RESTART: F:/Amala Varghese/My Captain-Online Course/AI (April Course)/3.Deleting different dictionary elements/Method-using a list of keys.py
+{2: 'For'}
+>>> 
